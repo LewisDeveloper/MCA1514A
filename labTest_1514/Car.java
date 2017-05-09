11abd0784f32457fa709a72651810d63177c0075
lewis1514 <sml@gmail.com>
2017-05-09 11:56:08 +0530

    Hello Car

---
+++ labTest_1514/Car.java
@@ -0,0 +1,13 @@
+package labTest_1514;
+
+public class Car {
+	public int engCapacity;
+	
+	Car(int engCapacity){
+		this.engCapacity = engCapacity;
+	}
+	
+	int getEngineCapacity(){
+		return engCapacity;
+	}
+}
---
+++ labTest_1514/CarTest.java
@@ -0,0 +1,14 @@
+package labTest_1514;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CarTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
